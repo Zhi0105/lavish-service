a485ef6d409f18c719e2fbfa8247e003757ebfbb
Argie <argie.b@flowerstore.ph>
2024-07-25 16:00:16 +0800

    modify main

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,6 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true, whitelist: true }));
-  if(process.env.API === 'DEV') {
-    await app.listen(3000);
-  }
-
-  if(process.env.API === 'PROD') {
-    await app.listen(process.env.PORT);
-  }
+  await app.listen(3000);
 }
 bootstrap();
